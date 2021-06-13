7eca3bcacdf25d9be2bff2508a879c51f34649f0
Pablo Martin <pabloenjerez@gmail.com>
2021-06-13 18:35:34 +0200

    Mostramos un campo de las monedas con algún estilo, hasta 21:44
    	modificado:     App.js
    	nuevo archivo:  components/CoinItem.js

--- App.js
+++ App.js
@@ -1,24 +1,46 @@
-import React, { useEffect } from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
+
+import CoinItem from "./components/CoinItem";
 
 const App = () => {
+
+  const [coins, setCoins] = useState([])
 
   const loadData = async () => {
     const res = await fetch(
       'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false'
     );
     const data = await res.json()
-    console.log(data)
+    setCoins(data)
   }
+
   useEffect(() => {
     loadData()
   }, [])
 
   return (
-    <View>
-      <Text>Hola mundo</Text>
+    <View style={styles.container} >
+      <FlatList
+        data={coins}
+        renderItem={({item}) => {
+          return <Text style={styles.inputGroup}>{item.name}</Text>
+        } }
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#141414',
+    alignItems: 'center',
+    flex: 1,
+    //padding: 35
+  },
+  inputGroup: {
+    color: 'white'
+  }
+})
 
 export default App
---
+++ components/CoinItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const CoinItem = () => {
+  return (
+    <View>
+      <Text>Coin</Text>
+    </View>
+  )
+}
+
+export default CoinItem
